722704895716d2130705dc2a05706e3c61271868
Aryan Kumar <legendxaryan18@gmail.com>
2026-06-26 23:18:20 +0530

    Create 1_Symmetric_Tree.cpp

---
+++ Week_4/Day_5/1_Symmetric_Tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if (left == NULL && right == NULL)
+            return true;
+
+        if (left == NULL || right == NULL)
+            return false;
+
+        return (left->val == right->val) &&
+               isMirror(left->left, right->right) &&
+               isMirror(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL)
+            return true;
+
+        return isMirror(root->left, root->right);
+    }
+};
