a01e3cc1e3e7b1f63b83347fa2e9e9d898e1be2b
Aryan Kumar <legendxaryan18@gmail.com>
2026-06-28 22:07:31 +0530

    Create ToTestInt.java

---
+++ Week_4/OOPS/ToTestInt.java
@@ -0,0 +1,16 @@
+interface Test {
+    void square(int n);
+}
+
+class Arithmetic implements Test {
+    public void square(int n) {
+        System.out.println("Square = " + (n * n));
+    }
+}
+
+public class ToTestInt {
+    public static void main(String[] args) {
+        Arithmetic obj = new Arithmetic();
+        obj.square(5);
+    }
+}
